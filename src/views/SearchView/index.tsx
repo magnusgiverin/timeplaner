d6a708379dbac14c296e228e0cc5d8376f703d24
Magnus Giverin <magnusgiverin@icloud.com>
2024-01-06 21:58:43 +0000

    I have decided i will wait now

--- src/views/SearchView/index.tsx
+++ src/views/SearchView/index.tsx
@@ -12,11 +12,8 @@ const Search = () => {
     const [showAll, setShowAll] = useState<boolean>(false); // Show all programs or only the first 20
     const router = useRouter();
     
-    console.log("HERE");
-    console.log(language)
     // Use useQuery directly within the functional component
     const result = api.program.programListByLang.useQuery(language);
-    console.log(result)
 
     // useEffect to handle side effects
     useEffect(() => {
