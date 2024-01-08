ede6fcc7be1dfec03c1fe441614b57ccf33b3e07
Magnus Giverin <magnusgiverin@icloud.com>
2024-01-08 21:30:41 +0000

    Revert back to original config

--- src/env.js
+++ src/env.js
@@ -9,7 +9,11 @@ export const env = createEnv({
   server: {
     DATABASE_URL: z
       .string()
-      .url(),
+      .url()
+      .refine(
+        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+        "You forgot to change the default URL"
+      ),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
