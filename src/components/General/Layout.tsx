1eedb2688e01510728400d88f5f219e10e01d9fb
Magnus Giverin <magnusgiverin@icloud.com>
2024-01-14 14:01:44 +0000

    Better phone view now

--- src/components/General/Layout.tsx
+++ src/components/General/Layout.tsx
@@ -1,16 +1,19 @@
 import type { ReactNode } from 'react';
 import Header from './Navbar';
+import { useMedia } from 'react-use';
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const isSmallScreen = useMedia('(max-width: 600px)'); // Adjust the maximum width as needed
+
   return (
     <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex w-full flex-col items-center mb-10">
-        <div className="w-2/3">{children}</div>
+        {isSmallScreen ? (<div className="w-full p-6">{children}</div>) : (<div className="w-2/3">{children}</div>)}
       </main>
     </div>
   );
