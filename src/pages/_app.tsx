ece14976bde8dc787203aad010d150047fe86bd4
Magnus Giverin <magnusgiverin@icloud.com>
2024-01-15 20:40:57 +0000

    Added analystics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,11 +7,13 @@ import { AppContextProvider } from "../contexts/appContext";
 import { LanguageProvider } from "~/contexts/languageContext";
 import { CalendarContextProvider } from "~/contexts/calendarContext";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from '@vercel/analytics/react';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <SpeedInsights />
+      <Analytics />
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
       </Head>
